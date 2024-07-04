2332074c66cfd732512d1c1ab882e248c8f70b23
Joao Pedro <joaooawk@gmail.com>
2024-07-04 14:16:21 -0300

    Melhoria no determinante Deixando o código menos verboso Melhoria no código de determinante

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "include/matriz.hpp"
 
 int main() {
-    Matriz m({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-    std::cout << m.det();
+    Matriz m({{1, 2}, {3, 4}});
+    std::cout << m.det(); // Teste de determinante
     return 0;
 }
